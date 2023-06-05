cc1765c8ff33da13720ec9c6ebd532ace87a4316
itrealms <it@rcrealms.org>
2023-06-05 12:42:47 -0700

    updates

--- JS/DATA/camp_data.js
+++ JS/DATA/camp_data.js
@@ -1,6 +1,189 @@
+// Session 1 Data //
+////////////////////////////////////////////////////////////////////////////////
+var session_1_class_1 = {
+	'camp_name': 'Reading & Writing Workshop2',
+	'age_group': '5-8 yrs',
+	'days': 'M, T, W, Th',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_2 = {
+	'camp_name': 'Math & Science Club',
+	'age_group': '9-13 yrs',
+	'days': 'M, T, W, Th',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_3 = {
+	'camp_name': 'PBIS Video Class',
+	'age_group': '9-13 yrs',
+	'days': 'M',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_4 = {
+	'camp_name': 'Music Theory Games',
+	'age_group': '6-13 yrs',
+	'days': 'T, W, Th*',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_5 = {
+	'camp_name': '',
+	'age_group': '',
+	'days': '',
+	'time': '',
+	'camp_size': ''
+};
+var session_1_class_6 = {
+	'camp_name': '',
+	'age_group': '',
+	'days': '',
+	'time': '',
+	'camp_size': ''
+};
+
+
+// Session 2 Data //
+////////////////////////////////////////////////////////////////////////////////
+var session_2_class_1 = {
+	'camp_name': 'Reading & Writing Workshop2',
+	'age_group': '5-8 yrs',
+	'days': 'M, T, W, Th',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_2_class_2 = {
+	'camp_name': 'Math & Science Club',
+	'age_group': '9-13 yrs',
+	'days': 'M, T, W, Th',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_2_class_3 = {
+	'camp_name': 'PBIS Video Class',
+	'age_group': '9-13 yrs',
+	'days': 'M',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_2_class_4 = {
+	'camp_name': 'Music Theory Games',
+	'age_group': '6-13 yrs',
+	'days': 'T, W, Th*',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_2_class_5 = {
+	'camp_name': '',
+	'age_group': '',
+	'days': '',
+	'time': '',
+	'camp_size': ''
+};
+var session_2_class_6 = {
+	'camp_name': '',
+	'age_group': '',
+	'days': '',
+	'time': '',
+	'camp_size': ''
+};
+
+
+// Session 3 Data //
+////////////////////////////////////////////////////////////////////////////////
+var session_1_class_1 = {
+	'camp_name': 'Reading & Writing Workshop2',
+	'age_group': '5-8 yrs',
+	'days': 'M, T, W, Th',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_2 = {
+	'camp_name': 'Math & Science Club',
+	'age_group': '9-13 yrs',
+	'days': 'M, T, W, Th',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_3 = {
+	'camp_name': 'PBIS Video Class',
+	'age_group': '9-13 yrs',
+	'days': 'M',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_4 = {
+	'camp_name': 'Music Theory Games',
+	'age_group': '6-13 yrs',
+	'days': 'T, W, Th*',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_5 = {
+	'camp_name': '',
+	'age_group': '',
+	'days': '',
+	'time': '',
+	'camp_size': ''
+};
+var session_1_class_6 = {
+	'camp_name': '',
+	'age_group': '',
+	'days': '',
+	'time': '',
+	'camp_size': ''
+};
+
+
+// Session 4 Data //
+////////////////////////////////////////////////////////////////////////////////
+var session_1_class_1 = {
+	'camp_name': 'Reading & Writing Workshop2',
+	'age_group': '5-8 yrs',
+	'days': 'M, T, W, Th',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_2 = {
+	'camp_name': 'Math & Science Club',
+	'age_group': '9-13 yrs',
+	'days': 'M, T, W, Th',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_3 = {
+	'camp_name': 'PBIS Video Class',
+	'age_group': '9-13 yrs',
+	'days': 'M',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_4 = {
+	'camp_name': 'Music Theory Games',
+	'age_group': '6-13 yrs',
+	'days': 'T, W, Th*',
+	'time': '8:30 am - 10:00 am',
+	'camp_size': 'CLOSED'
+};
+var session_1_class_5 = {
+	'camp_name': '',
+	'age_group': '',
+	'days': '',
+	'time': '',
+	'camp_size': ''
+};
+var session_1_class_6 = {
+	'camp_name': '',
+	'age_group': '',
+	'days': '',
+	'time': '',
+	'camp_size': ''
+};
+
+
 // Course Description Data //
 ////////////////////////////////////////////////////////////////////////////////
-
 var read_write = {'description': 'Do you love reading? How about writing? If you want to grow in your ability to do both, please join us for the Reading & Writing Workshop. We will be spending about 30 minutes each day reading in a variety of formats: individually, partner, small group, and whole class. We will also have about an hour each day to dive into some fun and engaging writing activities, such as writing about the books we are reading, autobiographies, interviewing family members and writing biographies, poetry, short stories, and plays which can be acted out. This class will be an opportunity to use many of the ELA skills students acquire during the school year in a fun and engaging way'};
 
 var math_sci = {'description': 'If you love math and science activities, this is the class for you! We will be spending time each day engaged in a variety of activities using mathematical skills and concepts such as measurement of distance, angles, and volume, as well as learning how to effectively estimate a measureable quantity. Additionally, students will be able to engage in scientific exploration of the world around them to better understand how the world operates and how that operation can be studied using scientific principles and with simple engaging experiments'};
