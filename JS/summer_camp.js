949d415134f65629d03e734e3d021497b6024cb4
itrealms <it@rcrealms.org>
2023-06-05 11:43:37 -0700

    update

--- JS/summer_camp.js
+++ JS/summer_camp.js
@@ -1,10 +1,12 @@
 $(function() {
+    // Display Course Info
+    /////////////////////////////////////////////////////////
     w3.displayObject("session_1_class_1", session_1_class_1);
     w3.displayObject("session_1_class_2", session_1_class_2);
     w3.displayObject("session_1_class_3", session_1_class_3);
     w3.displayObject("session_1_class_4", session_1_class_4);
     w3.displayObject("session_1_class_5", session_1_class_5);
-    w3.displayObject("session_1_class_6", session_1_class_6);
+    w3.displayObject("session_1_class_6", session_1_class_6)
 
     w3.displayObject("session_2_class_1", session_2_class_1);
     w3.displayObject("session_2_class_2", session_2_class_2);
@@ -26,4 +28,23 @@ $(function() {
     w3.displayObject("session_4_class_4", session_4_class_4);
     w3.displayObject("session_4_class_5", session_4_class_5);
     w3.displayObject("session_4_class_6", session_4_class_6);
+
+
+    // Display Course Descriptions
+    /////////////////////////////////////////////////////////
+    w3.displayObject("read_write", read_write);
+    w3.displayObject("math_sci", math_sci);
+    w3.displayObject("pbis_vid", pbis_vid);
+    w3.displayObject("music_games", music_games);
+    w3.displayObject("ukulele", ukulele);
+    w3.displayObject("art", art)
+
+    w3.displayObject("spanish", spanish);
+    w3.displayObject("yoga", yoga);
+    w3.displayObject("wood", wood);
+    w3.displayObject("guitar", guitar);
+    w3.displayObject("shop", shop);
+    w3.displayObject("orchestra", orchestra);
+
+    w3.displayObject("jamily", jamily);
 });
