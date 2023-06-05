5bc41b446ded5ff49b7fb3376c01b15e5685fff6
itrealms <it@rcrealms.org>
2023-06-05 10:26:19 -0700

    updates

---
+++ JS/DATA/camp_session 4.js
@@ -0,0 +1,51 @@
+// Session 1 Data //
+////////////////////////////////////////////////////////////////////////////////
+
+var session_4_class_1 = {
+'camp_name': 'Reading & Writing Workshop',
+'age_group': '5-8 yrs',
+'days': 'M, T, W, Th',
+'time': '8:30 am - 10:00 am',
+'camp_size': 'CLOSED',
+'description': 'Do you love reading? How about writing? If you want to grow in your ability to do both, please join us for the Reading & Writing Workshop. We will be spending about 30 minutes each day reading in a variety of formats: individually, partner, small group, and whole class. We will also have about an hour each day to dive into some fun and engaging writing activities, such as writing about the books we are reading, autobiographies, interviewing family members and writing biographies, poetry, short stories, and plays which can be acted out. This class will be an opportunity to use many of the ELA skills students acquire during the school year in a fun and engaging way'
+};
+var session_4_class_2 = {
+'camp_name': 'Math & Science Club',
+'age_group': '9-13 yrs',
+'days': 'M, T, W, Th',
+'time': '8:30 am - 10:00 am',
+'camp_size': 'CLOSED',
+'description': 'If you love math and science activities, this is the class for you! We will be spending time each day engaged in a variety of activities using mathematical skills and concepts such as measurement of distance, angles, and volume, as well as learning how to effectively estimate a measureable quantity. Additionally, students will be able to engage in scientific exploration of the world around them to better understand how the world operates and how that operation can be studied using scientific principles and with simple engaging experiments'
+};
+var session_4_class_3 = {
+'camp_name': 'PBIS Video Class',
+'age_group': '9-13 yrs',
+'days': 'M',
+'time': '8:30 am - 10:00 am',
+'camp_size': 'CLOSED',
+'description': 'Please join us in the design, production, and editing of a series of videos to be used in the upcoming school year to introduce REALMS school-wide expectations for our revamped PBIS program. Students will be provided with all necessary equipment and software by the teacher to do the work. Students will learn the steps involved in producing several videos from conceptual ideas to the planning stages through developing the actual video and still footage and finally to editing and post-production work. If you have experience with video production or want to learn something new, please join us for this 8 week-long project'
+};
+var session_4_class_4 = {
+'camp_name': 'Music Theory Games',
+'age_group': '6-13 yrs',
+'days': 'T, W, Th*',
+'time': '8:30 am - 10:00 am',
+'camp_size': 'CLOSED',
+'description': 'Children will have fun learning about the music alphabet, dynamics, rhythms, note reading, and more thru games designed for elementary children!<br>**NOTE**<br>June session starts: June 13th<br>July Session starts: July 11th'
+};
+var session_4_class_5 = {
+'camp_name': 'Music Theory Games',
+'age_group': '6-13 yrs',
+'days': 'T, W, Th*',
+'time': '8:30 am - 10:00 am',
+'camp_size': 'CLOSED',
+'description': 'Children will have fun learning about the music alphabet, dynamics, rhythms, note reading, and more thru games designed for elementary children!<br>**NOTE**<br>June session starts: June 13th<br>July Session starts: July 11th'
+};
+var session_4_class_6 = {
+'camp_name': 'Music Theory Games',
+'age_group': '6-13 yrs',
+'days': 'T, W, Th*',
+'time': '8:30 am - 10:00 am',
+'camp_size': 'CLOSED',
+'description': 'Children will have fun learning about the music alphabet, dynamics, rhythms, note reading, and more thru games designed for elementary children!<br>**NOTE**<br>June session starts: June 13th<br>July Session starts: July 11th'
+};
---
+++ JS/summer_camp.js
@@ -0,0 +1,4 @@
+$(function() {
+	w3.displayObject("data_b1c1", data_b1c1);
+	w3.displayObject("data_b2c2", data_b2c2);
+});
