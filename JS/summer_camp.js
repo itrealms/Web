a5a52d6bf31313fc614f0a47899e8b904339efec
itrealms <it@rcrealms.org>
2023-06-06 09:45:23 -0700

    updates

--- JS/summer_camp.js
+++ JS/summer_camp.js
@@ -1,47 +1,61 @@
 $(function() {
     // Display Course Info
     /////////////////////////////////////////////////////////
-    w3.displayObject("session_1_class_1", session_1_class_1);
-    w3.displayObject("session_1_class_2", session_1_class_2);
-    w3.displayObject("session_1_class_3", session_1_class_3);
-    w3.displayObject("session_1_class_4", session_1_class_4);
-    w3.displayObject("session_1_class_5", session_1_class_5);
-    w3.displayObject("session_1_class_6", session_1_class_6);
+    try {w3.displayObject("session_1_class_1", session_1_class_1);}catch(err){}
+    try {w3.displayObject("session_1_class_2", session_1_class_2);}catch(err){}
+    try {w3.displayObject("session_1_class_3", session_1_class_3);}catch(err){}
+    try {w3.displayObject("session_1_class_4", session_1_class_4);}catch(err){}
+    try {w3.displayObject("session_1_class_5", session_1_class_5);}catch(err){}
+    try {w3.displayObject("session_1_class_6", session_1_class_6);}catch(err){}
 
-    w3.displayObject("session_2_class_1", session_2_class_1);
-    w3.displayObject("session_2_class_2", session_2_class_2);
-    w3.displayObject("session_2_class_3", session_2_class_3);
-    w3.displayObject("session_2_class_4", session_2_class_4);
-    w3.displayObject("session_2_class_5", session_2_class_5);
-    w3.displayObject("session_2_class_6", session_2_class_6);
+    try {w3.displayObject("session_2_class_1", session_2_class_1);}catch(err){}
+    try {w3.displayObject("session_2_class_2", session_2_class_2);}catch(err){}
+    try {w3.displayObject("session_2_class_3", session_2_class_3);}catch(err){}
+    try {w3.displayObject("session_2_class_4", session_2_class_4);}catch(err){}
+    try {w3.displayObject("session_2_class_5", session_2_class_5);}catch(err){}
+    try {w3.displayObject("session_2_class_6", session_2_class_6);}catch(err){}
 
-    w3.displayObject("session_3_class_1", session_3_class_1);
-    w3.displayObject("session_3_class_2", session_3_class_2);
-    w3.displayObject("session_3_class_3", session_3_class_3);
-    w3.displayObject("session_3_class_4", session_3_class_4);
-    w3.displayObject("session_3_class_5", session_3_class_5);
-    w3.displayObject("session_3_class_6", session_3_class_6);
+    try {w3.displayObject("session_3_class_1", session_3_class_1);}catch(err){}
+    try {w3.displayObject("session_3_class_2", session_3_class_2);}catch(err){}
+    try {w3.displayObject("session_3_class_3", session_3_class_3);}catch(err){}
+    try {w3.displayObject("session_3_class_4", session_3_class_4);}catch(err){}
+    try {w3.displayObject("session_3_class_5", session_3_class_5);}catch(err){}
+    try {w3.displayObject("session_3_class_6", session_3_class_6);}catch(err){}
 
-    w3.displayObject("session_4_class_1", session_4_class_1);
-    w3.displayObject("session_4_class_2", session_4_class_2);
-    w3.displayObject("session_4_class_3", session_4_class_3);
-    w3.displayObject("session_4_class_4", session_4_class_4);
-    w3.displayObject("session_4_class_5", session_4_class_5);
-    w3.displayObject("session_4_class_6", session_4_class_6);
+    try {w3.displayObject("session_4_class_1", session_4_class_1);}catch(err){}
+    try {w3.displayObject("session_4_class_2", session_4_class_2);}catch(err){}
+    try {w3.displayObject("session_4_class_3", session_4_class_3);}catch(err){}
+    try {w3.displayObject("session_4_class_4", session_4_class_4);}catch(err){}
+    try {w3.displayObject("session_4_class_5", session_4_class_5);}catch(err){}
+    try {w3.displayObject("session_4_class_6", session_4_class_6);}catch(err){}
 
     // Display Course Descriptions
     /////////////////////////////////////////////////////////
-    w3.displayObject("read_write", read_write);
-    w3.displayObject("math_sci", math_sci);
-    w3.displayObject("pbis_vid", pbis_vid);
-    w3.displayObject("music_games", music_games);
-    w3.displayObject("ukulele", ukulele);
-    w3.displayObject("art", art);
-    w3.displayObject("spanish", spanish);
-    w3.displayObject("yoga", yoga);
-    w3.displayObject("wood", wood);
-    w3.displayObject("guitar", guitar);
-    w3.displayObject("shop", shop);
-    w3.displayObject("orchestra", orchestra);
-    w3.displayObject("jamily", jamily);
+    try {w3.displayObject("session_1_class_1_desc", read_write);}catch(err){}
+    try {w3.displayObject("session_1_class_2_desc", math_sci);}catch(err){}
+    try {w3.displayObject("session_1_class_3_desc", pbis_vid);}catch(err){}
+    try {w3.displayObject("session_1_class_4_desc", music_games);}catch(err){}
+    try {w3.displayObject("session_1_class_5_desc", );}catch(err){}
+    try {w3.displayObject("session_1_class_6_desc", );}catch(err){}
+
+    try {w3.displayObject("session_2_class_1_desc", read_write);}catch(err){}
+    try {w3.displayObject("session_2_class_2_desc", math_sci);}catch(err){}
+    try {w3.displayObject("session_2_class_3_desc", ukulele);}catch(err){}
+    try {w3.displayObject("session_2_class_4_desc", );}catch(err){}
+    try {w3.displayObject("session_2_class_5_desc", );}catch(err){}
+    try {w3.displayObject("session_2_class_6_desc", );}catch(err){}
+
+    try {w3.displayObject("session_3_class_1_desc", art);}catch(err){}
+    try {w3.displayObject("session_3_class_2_desc", spanish);}catch(err){}
+    try {w3.displayObject("session_3_class_3_desc", yoga);}catch(err){}
+    try {w3.displayObject("session_3_class_4_desc", wood);}catch(err){}
+    try {w3.displayObject("session_3_class_5_desc", guitar);}catch(err){}
+    try {w3.displayObject("session_3_class_6_desc", );}catch(err){}
+
+    try {w3.displayObject("session_4_class_1_desc", art);}catch(err){}
+    try {w3.displayObject("session_4_class_2_desc", spanish);}catch(err){}
+    try {w3.displayObject("session_4_class_3_desc", shop);}catch(err){}
+    try {w3.displayObject("session_4_class_4_desc", orchestra);}catch(err){}
+    try {w3.displayObject("session_4_class_5_desc", jamily);}catch(err){}
+    try {w3.displayObject("session_4_class_6_desc", );}catch(err){}
 });
