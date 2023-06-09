dafb55061d6765ee0a408d9b39dd19929af40c74
itrealms <it@rcrealms.org>
2023-06-09 08:17:56 -0700

    update

--- JS/summer_camp.js
+++ JS/summer_camp.js
@@ -1,6 +1,13 @@
-$(function() {
-    // Display Course Info
-    /////////////////////////////////////////////////////////
+ $(function() {
+    // Course Catalog
+    try {w3.displayObject("arts", arts);}catch(err){}
+    try {w3.displayObject("core", core);}catch(err){}
+    try {w3.displayObject("language", language);}catch(err){}
+    try {w3.displayObject("music", music);}catch(err){}
+    try {w3.displayObject("video", video);}catch(err){}
+    try {w3.displayObject("wellness", wellness);}catch(err){}
+
+    // Course Calendar
     try {w3.displayObject("session_1_class_1", session_1_class_1);}catch(err){}
     try {w3.displayObject("session_1_class_2", session_1_class_2);}catch(err){}
     try {w3.displayObject("session_1_class_3", session_1_class_3);}catch(err){}
@@ -28,34 +35,4 @@ $(function() {
     try {w3.displayObject("session_4_class_4", session_4_class_4);}catch(err){}
     try {w3.displayObject("session_4_class_5", session_4_class_5);}catch(err){}
     try {w3.displayObject("session_4_class_6", session_4_class_6);}catch(err){}
-
-    // Display Course Descriptions
-    /////////////////////////////////////////////////////////
-    try {w3.displayObject("session_1_class_1_desc", read_write);}catch(err){}
-    try {w3.displayObject("session_1_class_2_desc", math_sci);}catch(err){}
-    try {w3.displayObject("session_1_class_3_desc", pbis_vid);}catch(err){}
-    try {w3.displayObject("session_1_class_4_desc", music_games);}catch(err){}
-    try {w3.displayObject("session_1_class_5_desc", );}catch(err){}
-    try {w3.displayObject("session_1_class_6_desc", );}catch(err){}
-
-    try {w3.displayObject("session_2_class_1_desc", read_write);}catch(err){}
-    try {w3.displayObject("session_2_class_2_desc", math_sci);}catch(err){}
-    try {w3.displayObject("session_2_class_3_desc", ukulele);}catch(err){}
-    try {w3.displayObject("session_2_class_4_desc", );}catch(err){}
-    try {w3.displayObject("session_2_class_5_desc", );}catch(err){}
-    try {w3.displayObject("session_2_class_6_desc", );}catch(err){}
-
-    try {w3.displayObject("session_3_class_1_desc", art);}catch(err){}
-    try {w3.displayObject("session_3_class_2_desc", spanish);}catch(err){}
-    try {w3.displayObject("session_3_class_3_desc", yoga);}catch(err){}
-    try {w3.displayObject("session_3_class_4_desc", wood);}catch(err){}
-    try {w3.displayObject("session_3_class_5_desc", guitar);}catch(err){}
-    try {w3.displayObject("session_3_class_6_desc", );}catch(err){}
-
-    try {w3.displayObject("session_4_class_1_desc", art);}catch(err){}
-    try {w3.displayObject("session_4_class_2_desc", spanish);}catch(err){}
-    try {w3.displayObject("session_4_class_3_desc", shop);}catch(err){}
-    try {w3.displayObject("session_4_class_4_desc", orchestra);}catch(err){}
-    try {w3.displayObject("session_4_class_5_desc", jamily);}catch(err){}
-    try {w3.displayObject("session_4_class_6_desc", );}catch(err){}
-});
+ });
