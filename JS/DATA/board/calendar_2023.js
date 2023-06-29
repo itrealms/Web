dc716482c7ea89305d34fb135d9c28eb4cb89dbc
itrealms <115303267+itrealms@users.noreply.github.com>
2023-06-29 10:23:26 -0700

    board data redesign

---
+++ JS/DATA/board/calendar_2023.js
@@ -0,0 +1,20 @@
+const jan2023 = {"jan" : [
+	{
+        "agenda" : "https://drive.google.com/file/d/1XhEEAyDbaKCvEd_16F7qCXk7j-ZfbUvJ/view",
+        "agenda_date" : "2023-01-12 - Special Agenda",
+        "minutes" : "https://drive.google.com/file/d/1sdRqZ8UC6IyfZgaF8o0RKlLzYIzWxsUE/view",
+        "minutes_date" : "2023-01-12 - Special Minutes",
+    },
+	{
+        "agenda" : "https://drive.google.com/file/d/1abY13kCPWWB4BpDOyA-x1vziy291m9yh/view",
+        "agenda_date" : "2023-01-23 - Agenda",
+        "minutes" : "https://drive.google.com/file/d/1VGFT-OddqqMR4ONCOmUynaXcLQ0XP4b4/view",
+        "minutes_date" : "2023-01-23 - Minutes",
+    },
+    {
+        "agenda" : "https://drive.google.com/file/d/1g1m76oyMl2Dl4lAPzY4wZjlsVbKmku8w/view",
+        "agenda_date" : "2023-01-30 - Special Agenda",
+        "minutes" : "https://drive.google.com/file/d/1tBcRB_-2e0vAQg3I8HuT-_vtqqRiEyWU/view",
+        "minutes_date" : "2023-01-30 - Special Minutes",
+    }
+]};
