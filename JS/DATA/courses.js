5c5744bc37c3e542617bf0597dab550e64ce326d
itrealms <115303267+itrealms@users.noreply.github.com>
2023-06-29 09:36:40 -0700

    var to const

--- JS/DATA/courses.js
+++ JS/DATA/courses.js
@@ -1,13 +1,13 @@
 // Arts & Crafts Data //
 ////////////////////////////////////////////////////////////////////////////////
-var arts = {"courses" : [
+const arts = {"courses" : [
 	{"CourseName" : "Art",
 	"Description" : "Children will have a chance to bring out their artistic side and learn how to draw, paint, etc!"}
 ]};
 
 // Core Learning Data //
 ////////////////////////////////////////////////////////////////////////////////
-var core = {"courses" : [
+const core = {"courses" : [
 	{"CourseName" : "Math & Science Club",
   "Description" : "If you love math and science activities, this is the class for you! We will be spending time each day engaged in a variety of activities using mathematical skills and concepts such as measurement of distance, angles, and volume, as well as learning how to effectively estimate a measureable quantity. Additionally, students will be able to engage in scientific exploration of the world around them to better understand how the world operates and how that operation can be studied using scientific principles and with simple engaging experiments"},
 	{"CourseName" : "Reading & Writing Workshop",
@@ -16,14 +16,14 @@ var core = {"courses" : [
 
 // Language Data //
 ////////////////////////////////////////////////////////////////////////////////
-var language = {"courses" : [
+const language = {"courses" : [
 	{"CourseName" : "Spanish Club",
   "Description" : "Hola! Tu quieres hablar en espanol? Then join us for some fun and engaging Spanish lessons and activities. Each 90 minute class will be a combination of Spanish language lessons for about 30 minutes and opportunities to read, listen, and speak in Spanish for about 60 minutes. We will have a chance to explore Spanish food, music, videos and geography as a way for students to engage with the Spanish they study all year long at REALMS. Students will have an opportunity to learn more about Spanish speaking culture while growing in their language skills"}
 ]};
 
 // Music Data //
 ////////////////////////////////////////////////////////////////////////////////
-var music = {"courses" : [
+const music = {"courses" : [
 	{"CourseName" : "Beginning Guitar",
   "Description" : "This class is for children with little to no experience. Children will have fun learning the instrument strings, how to tune, learn single notes and chords!<br>**NOTE**<br>June session: June 13 to June 29<br>July session: July 11 to July 27"},
 	{"CourseName" : "Beginning Orchestra",
@@ -38,14 +38,14 @@ var music = {"courses" : [
 
 // Video Production Data //
 ////////////////////////////////////////////////////////////////////////////////
-var video = {"courses" : [
+const video = {"courses" : [
 	{"CourseName" : "PBIS Video Workshop",
   "Description" : "Please join us in the design, production, and editing of a series of videos to be used in the upcoming school year to introduce REALMS school-wide expectations for our revamped PBIS program. Students will be provided with all necessary equipment and software by the teacher to do the work. Students will learn the steps involved in producing several videos from conceptual ideas to the planning stages through developing the actual video and still footage and finally to editing and post-production work. If you have experience with video production or want to learn something new, please join us for this 8 week-long project"}
 ]};
 
 // Wellness Data //
 ////////////////////////////////////////////////////////////////////////////////
-var wellness = {"courses" : [
+const wellness = {"courses" : [
 	{"CourseName" : "Yoga",
   "Description" : "Are you in need of more flexibility and strength? Are you looking for a new type of exercise that you have never tried before? Then come and give yoga a try! Yoga is great for not only improved flexibility and muscle tone, but it also has a meditative aspect that allows practitioners to calm their minds for greater clarity of their surroundings and mind-body connectedness. Yoga is a wonderful addition to any other sport you may be engaged in throughout your week. Come and try yoga as something new!"}
 ]};
