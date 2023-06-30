f1332125bf50a25f0ea31895b1bd6e62b289653b
itrealms <it@rcrealms.org>
2023-06-30 11:14:00 -0700

    updates

---
+++ JS/DATA/board/archive/board_archive.js
@@ -0,0 +1,16 @@
+$(function() {
+  try {w3.displayObject("jan2019", jan2019);}catch(err){}
+  try {w3.displayObject("feb2019", feb2019);}catch(err){}
+  try {w3.displayObject("mar2019", mar2019);}catch(err){}
+  try {w3.displayObject("apr2019", apr2019);}catch(err){}
+  try {w3.displayObject("may2019", may2019);}catch(err){}
+  try {w3.displayObject("jun2019", jun2019);}catch(err){}
+  try {w3.displayObject("jul2019", jul2019);}catch(err){}
+  try {w3.displayObject("aug2019", aug2019);}catch(err){}
+  try {w3.displayObject("sep2019", sep2019);}catch(err){}
+  try {w3.displayObject("oct2019", oct2019);}catch(err){}
+  try {w3.displayObject("nov2019", nov2019);}catch(err){}
+  try {w3.displayObject("dec2019", dec2019);}catch(err){}
+
+  $('a[href="#"]').removeAttr("href");
+});
---
+++ JS/DATA/board/archive/calendar_2019.js
@@ -0,0 +1,137 @@
+const jan2019 = {"jan" : [
+	{
+		"agenda" : "#",
+		"agenda_date" : "",
+		"minutes" : "#",
+		"minutes_date" : ""
+	}
+]};
+
+const feb2019 = {"feb" : [
+	{
+		"agenda" : "#",
+		"agenda_date" : "",
+		"minutes" : "#",
+		"minutes_date" : ""
+	}
+]};
+
+const mar2019 = {"mar" : [
+	{
+		"agenda" : "#",
+		"agenda_date" : "",
+		"minutes" : "#",
+		"minutes_date" : ""
+	}
+]};
+
+const apr2019 = {"apr" : [
+	{
+		"agenda" : "#",
+		"agenda_date" : "",
+		"minutes" : "#",
+		"minutes_date" : ""
+	}
+]};
+
+const may2019 = {"may" : [
+	{
+		"agenda" : "#",
+		"agenda_date" : "",
+		"minutes" : "#",
+		"minutes_date" : ""
+	}
+]};
+
+const jun2019 = {"jun" : [
+	{
+		"agenda" : "https://drive.google.com/file/d/1n7x2mm4g-hgr_l4sOIVohmGAQfmt_-_L/view",
+		"agenda_date" : "2019-06-12 - Special Agenda",
+		"minutes" : "#",
+		"minutes_date" : "2019-06-12 - Special Minutes"
+	},
+	{
+		"agenda" : "#",
+		"agenda_date" : "2019-06-26 - Agenda",
+		"minutes" : "https://drive.google.com/file/d/1zH1U1KvQH6tJEfq_g6WUKaeNoTkHJnuo/view",
+		"minutes_date" : "2019-06-26 - Minutes"
+	}
+]};
+
+const jul2019 = {"jul" : [
+	{
+		"agenda" : "#",
+		"agenda_date" : "",
+		"minutes" : "#",
+		"minutes_date" : ""
+	}
+]};
+
+const aug2019 = {"aug" : [
+	{
+		"agenda" : "#",
+		"agenda_date" : "2019-08-28 - Agenda",
+		"minutes" : "https://drive.google.com/file/d/1dMKp575H03jP4lSi28BXyllhbrHnIlV-/view",
+		"minutes_date" : "2019-08-28 - Minutes"
+	}
+]};
+
+const sep2019 = {"sep" : [
+	{
+		"agenda" : "https://drive.google.com/file/d/1GTSEykjm99J90oDJ4iZ5p_A6p8gP-J4h/view",
+		"agenda_date" : "2019-09-16 - Special Agenda",
+		"minutes" : "#",
+		"minutes_date" : "2019-09-16 - Special Minutes"
+	},
+	{
+		"agenda" : "https://drive.google.com/file/d/13Wz-OZ4Piz2-ttUBwJFQiMX8kFwZrkNG/view",
+		"agenda_date" : "2019-09-25 - Agenda",
+		"minutes" : "#",
+		"minutes_date" : "2019-09-25 - Minutes"
+	}
+]};
+
+const oct2019 = {"oct" : [
+	{
+		"agenda" : "https://drive.google.com/file/d/1DDXP4iT30lxCLfM19ylSpjYq7pm0uZFO/view",
+		"agenda_date" : "2019-10-11 - Special Agenda",
+		"minutes" : "#",
+		"minutes_date" : "2019-10-11 - Special Minutes"
+	},
+	{
+		"agenda" : "#",
+		"agenda_date" : "2019-10-23 - Agenda",
+		"minutes" : "https://drive.google.com/file/d/13bFHL86FCyF1ckx_1q3g9FgTCY8rYptg/view",
+		"minutes_date" : "2019-10-23 - Minutes"
+	}
+]};
+
+const nov2019 = {"nov" : [
+	{
+		"agenda" : "https://drive.google.com/file/d/1uQafgFWd6eva4bC7fXjgYCx-xHVYQj9U/view",
+		"agenda_date" : "2019-11-09 - Special Agenda",
+		"minutes" : "#",
+		"minutes_date" : "2019-11-09 - Special Minutes"
+	},
+	{
+		"agenda" : "#",
+		"agenda_date" : "2019-11-20 - Agenda",
+		"minutes" : "https://drive.google.com/file/d/1s-3QBfAnKdZwNE3DFjG4iZ701AeIE78H/view",
+		"minutes_date" : "2019-11-20 - Minutes"
+	}
+]};
+
+const dec2019 = {"dec" : [
+	{
+		"agenda" : "#",
+		"agenda_date" : "2019-12-11 - Special Agenda",
+		"minutes" : "https://drive.google.com/file/d/105zh1uJSDnA_Kra_6Y_usbrQtY-jvZ6m/view",
+		"minutes_date" : "2019-12-11 - Special Minutes"
+	},
+	{
+		"agenda" : "#",
+		"agenda_date" : "2019-12-18 - Agenda",
+		"minutes" : "https://drive.google.com/file/d/1Eadgkv_zSQm0XyRW8Fe8C-HXsZk06srG/view",
+		"minutes_date" : "2019-12-18 - Minutes"
+	}
+]};
