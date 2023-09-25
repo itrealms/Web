401aebaace8ed0507e5cd06608ae6fe55f77098f
itrealms <it@rcrealms.org>
2023-09-25 10:52:03 -0700

    updates

---
+++ JS/DATA/ELOP/elop.js
@@ -0,0 +1,38 @@
+ $(function() {
+    // Course Catalog
+    try {w3.displayObject("arts", arts);}catch(err){}
+    try {w3.displayObject("core", core);}catch(err){}
+    try {w3.displayObject("language", language);}catch(err){}
+    try {w3.displayObject("music", music);}catch(err){}
+    try {w3.displayObject("video", video);}catch(err){}
+    try {w3.displayObject("wellness", wellness);}catch(err){}
+
+    // Course Calendar
+    try {w3.displayObject("session_1_class_1", session_1_class_1);}catch(err){}
+    try {w3.displayObject("session_1_class_2", session_1_class_2);}catch(err){}
+    try {w3.displayObject("session_1_class_3", session_1_class_3);}catch(err){}
+    try {w3.displayObject("session_1_class_4", session_1_class_4);}catch(err){}
+    try {w3.displayObject("session_1_class_5", session_1_class_5);}catch(err){}
+    try {w3.displayObject("session_1_class_6", session_1_class_6);}catch(err){}
+
+    try {w3.displayObject("session_2_class_1", session_2_class_1);}catch(err){}
+    try {w3.displayObject("session_2_class_2", session_2_class_2);}catch(err){}
+    try {w3.displayObject("session_2_class_3", session_2_class_3);}catch(err){}
+    try {w3.displayObject("session_2_class_4", session_2_class_4);}catch(err){}
+    try {w3.displayObject("session_2_class_5", session_2_class_5);}catch(err){}
+    try {w3.displayObject("session_2_class_6", session_2_class_6);}catch(err){}
+
+    try {w3.displayObject("session_3_class_1", session_3_class_1);}catch(err){}
+    try {w3.displayObject("session_3_class_2", session_3_class_2);}catch(err){}
+    try {w3.displayObject("session_3_class_3", session_3_class_3);}catch(err){}
+    try {w3.displayObject("session_3_class_4", session_3_class_4);}catch(err){}
+    try {w3.displayObject("session_3_class_5", session_3_class_5);}catch(err){}
+    try {w3.displayObject("session_3_class_6", session_3_class_6);}catch(err){}
+
+    try {w3.displayObject("session_4_class_1", session_4_class_1);}catch(err){}
+    try {w3.displayObject("session_4_class_2", session_4_class_2);}catch(err){}
+    try {w3.displayObject("session_4_class_3", session_4_class_3);}catch(err){}
+    try {w3.displayObject("session_4_class_4", session_4_class_4);}catch(err){}
+    try {w3.displayObject("session_4_class_5", session_4_class_5);}catch(err){}
+    try {w3.displayObject("session_4_class_6", session_4_class_6);}catch(err){}
+ });
