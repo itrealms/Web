4400819ec6518463acad1480ff0602f90216f087
itrealms <115303267+itrealms@users.noreply.github.com>
2023-11-22 22:47:38 -0800

    Update calendar_2023.js

--- JS/DATA/board/calendar_2023.js
+++ JS/DATA/board/calendar_2023.js
@@ -1,11 +1,11 @@
 const jan2023 = {"jan" : [
-	{
+  {
     "agenda" : "https://drive.google.com/file/d/1XhEEAyDbaKCvEd_16F7qCXk7j-ZfbUvJ/view",
     "agenda_date" : "2023-01-12 - Special Agenda",
     "minutes" : "https://drive.google.com/file/d/1sdRqZ8UC6IyfZgaF8o0RKlLzYIzWxsUE/view",
     "minutes_date" : "2023-01-12 - Special Minutes"
   },
-	{
+  {
     "agenda" : "https://drive.google.com/file/d/1abY13kCPWWB4BpDOyA-x1vziy291m9yh/view",
     "agenda_date" : "2023-01-23 - Agenda",
     "minutes" : "https://drive.google.com/file/d/1VGFT-OddqqMR4ONCOmUynaXcLQ0XP4b4/view",
@@ -164,7 +164,7 @@ const nov2023 = {"nov" : [
   {
     "agenda" : "https://drive.google.com/file/d/1NtVWg59QV_SQslBYartX8YSlFaf_UhSV/view",
     "agenda_date" : "2023-11-28 - Agenda",
-    "minutes" : "",
+    "minutes" : "#",
     "minutes_date" : "2023-11-28 - Minutes"
   }
 ]};
