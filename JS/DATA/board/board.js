800ef845ef5b3a0793410292b491fcc1a5cfaca9
itrealms <115303267+itrealms@users.noreply.github.com>
2023-11-22 22:51:26 -0800

    Update board.js

--- JS/DATA/board/board.js
+++ JS/DATA/board/board.js
@@ -1,5 +1,5 @@
 $(function() {
-  try {w3.displayObject("jan2023", jan2023);}catch(err){}
+  try {w3.displayObject("jan2023", jan2023);}catch(err){alert(err)}
   try {w3.displayObject("feb2023", feb2023);}catch(err){}
   try {w3.displayObject("mar2023", mar2023);}catch(err){}
   try {w3.displayObject("apr2023", apr2023);}catch(err){}
