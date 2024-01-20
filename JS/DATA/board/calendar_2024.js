6372594337098ffbfb62aeeb5e6522c99b66b057
itrealms <115303267+itrealms@users.noreply.github.com>
2024-01-20 08:07:11 -0800

    Update calendar_2024.js

--- JS/DATA/board/calendar_2024.js
+++ JS/DATA/board/calendar_2024.js
@@ -2,7 +2,7 @@ const jan2024 = {"jan" : [
 	{
     "agenda" : "https://drive.google.com/file/d/1VnsRG4D8xnpO1E5Ilf2P8NxEJs-vnlc8/view",
     "agenda_date" : "2024-01-23 - Agenda",
-    "minutes" : "",
+    "minutes" : "#",
     "minutes_date" : "2024-01-23 - Minutes"
   }
 ]};
