c6bf807efd32d61d48e593fbfc906f61520605fc
itrealms <it@rcrealms.org>
2024-02-23 10:27:38 -0800

    updates

--- Pages/Site Council/calendar/council.js
+++ Pages/Site Council/calendar/council.js
@@ -12,18 +12,5 @@ $(function() {
   try {w3.displayObject("nov2024", nov2024);}catch(err){}
   try {w3.displayObject("dec2024", dec2024);}catch(err){}
 
-  try {w3.displayObject("jan2023", jan2023);}catch(err){}
-  try {w3.displayObject("feb2023", feb2023);}catch(err){}
-  try {w3.displayObject("mar2023", mar2023);}catch(err){}
-  try {w3.displayObject("apr2023", apr2023);}catch(err){}
-  try {w3.displayObject("may2023", may2023);}catch(err){}
-  try {w3.displayObject("jun2023", jun2023);}catch(err){}
-  try {w3.displayObject("jul2023", jul2023);}catch(err){}
-  try {w3.displayObject("aug2023", aug2023);}catch(err){}
-  try {w3.displayObject("sep2023", sep2023);}catch(err){}
-  try {w3.displayObject("oct2023", oct2023);}catch(err){}
-  try {w3.displayObject("nov2023", nov2023);}catch(err){}
-  try {w3.displayObject("dec2023", dec2023);}catch(err){}
-
   $('a[href="#"]').removeAttr("href");
 });
