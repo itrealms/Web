e695a335958be50166bad7a1dcca0f401df8b2fd
itrealms <it@rcrealms.org>
2024-02-23 10:15:10 -0800

    updates

---
+++ Pages/Site Council/calendar/calendar_2024.js
@@ -0,0 +1,107 @@
+const jan2024 = {"jan" : [
+	{
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const feb2024 = {"feb" : [
+  {
+    "agenda" : "https://drive.google.com/file/d/1tRRMwV5MVOBEeBWKqYH8hFGpOGAXMxOX/view",
+    "agenda_date" : "2024-02-23 - Agenda",
+    "minutes" : "#",
+    "minutes_date" : "2024-02-23 - Minutes"
+  }
+]};
+
+const mar2024 = {"mar" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const apr2024 = {"apr" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "2024-04-05 - Upcoming",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const may2024 = {"may" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "2024-05-03 - Upcoming",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const jun2024 = {"jun" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const jul2024 = {"jul" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const aug2024 = {"aug" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const sep2024 = {"sep" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const oct2024 = {"oct" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const nov2024 = {"nov" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const dec2024 = {"dec" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
