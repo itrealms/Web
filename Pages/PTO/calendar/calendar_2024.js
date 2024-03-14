2d3e9697e1d26851f6e6f93608c768cfd7fae37f
itrealms <it@rcrealms.org>
2024-03-14 09:21:14 -0700

    updates

---
+++ Pages/PTO/calendar/calendar_2024.js
@@ -0,0 +1,107 @@
+const jan2024 = {"jan" : [
+	{
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const feb2024 = {"feb" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const mar2024 = {"mar" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const apr2024 = {"apr" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "2024-04-05 - Upcoming",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const may2024 = {"may" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "2024-05-03 - Upcoming",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const jun2024 = {"jun" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const jul2024 = {"jul" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const aug2024 = {"aug" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const sep2024 = {"sep" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const oct2024 = {"oct" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const nov2024 = {"nov" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
+
+const dec2024 = {"dec" : [
+  {
+    "agenda" : "#",
+    "agenda_date" : "",
+    "minutes" : "#",
+    "minutes_date" : ""
+  }
+]};
---
+++ Pages/PTO/calendar/council.js
@@ -0,0 +1,16 @@
+$(function() {
+  try {w3.displayObject("jan2024", jan2024);}catch(err){}
+  try {w3.displayObject("feb2024", feb2024);}catch(err){}
+  try {w3.displayObject("mar2024", mar2024);}catch(err){}
+  try {w3.displayObject("apr2024", apr2024);}catch(err){}
+  try {w3.displayObject("may2024", may2024);}catch(err){}
+  try {w3.displayObject("jun2024", jun2024);}catch(err){}
+  try {w3.displayObject("jul2024", jul2024);}catch(err){}
+  try {w3.displayObject("aug2024", aug2024);}catch(err){}
+  try {w3.displayObject("sep2024", sep2024);}catch(err){}
+  try {w3.displayObject("oct2024", oct2024);}catch(err){}
+  try {w3.displayObject("nov2024", nov2024);}catch(err){}
+  try {w3.displayObject("dec2024", dec2024);}catch(err){}
+
+  $('a[href="#"]').removeAttr("href");
+});
