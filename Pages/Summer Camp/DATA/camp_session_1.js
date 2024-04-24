1c8e9b7a50f6119ba66411fe6f1fc8ead9766c5f
itrealms <it@rcrealms.org>
2024-04-24 09:50:04 -0700

    updates

--- Pages/Summer Camp/DATA/camp_session_1.js
+++ Pages/Summer Camp/DATA/camp_session_1.js
@@ -1,33 +1,33 @@
 // Session 1 Data //
 ////////////////////////////////////////////////////////////////////////////////
 var session_1_class_1 = {
-	'camp_name': 'Math & Science Club',
-	'age_group': '9-13 yrs',
-	'days': 'M, T, W, Th',
-	'time': '8:30 am - 10:00 am',
-	'camp_size': 'CLOSED'
+	camp_name: "Math & Science Club",
+	age_group: "9-13 yrs",
+	days: "M, T, W, Th",
+	time: "8:30 am - 10:00 am",
+	camp_size: "CLOSED"
 };
 
 var session_1_class_2 = {
-	'camp_name': 'Reading & Writing Workshop',
-	'age_group': '5-8 yrs',
-	'days': 'M, T, W, Th',
-	'time': '8:30 am - 10:00 am',
-	'camp_size': 'CLOSED'
+	camp_name: "Reading & Writing Workshop",
+	age_group: "5-8 yrs",
+	days: "M, T, W, Th",
+	time: "8:30 am - 10:00 am",
+	camp_size: "CLOSED"
 };
 
 var session_1_class_3 = {
-	'camp_name': 'PBIS Video Workshop',
-	'age_group': '9-13 yrs',
-	'days': 'M',
-	'time': '8:30 am - 10:00 am',
-	'camp_size': 'CLOSED'
+	camp_name: "PBIS Video Workshop",
+	age_group: "9-13 yrs",
+	days: "M",
+	time: "8:30 am - 10:00 am",
+	camp_size: "CLOSED"
 };
 
 var session_1_class_4 = {
-	'camp_name': 'Music Theory Games',
-	'age_group': '6-13 yrs',
-	'days': 'T, W, Th*',
-	'time': '8:30 am - 10:00 am',
-	'camp_size': 'CLOSED'
+	camp_name: "Music Theory Games",
+	age_group: "6-13 yrs",
+	days: "T, W, Th*",
+	time: "8:30 am - 10:00 am",
+	camp_size: "CLOSED"
 };
