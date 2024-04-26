0409ea07c40e5cb0cc1bb0c8f3f748df9ae6b062
itrealms <it@rcrealms.org>
2024-04-26 10:50:58 -0700

    updates

--- Pages/Summer Camp/DATA/camp_session_4.js
+++ Pages/Summer Camp/DATA/camp_session_4.js
@@ -1,18 +1,18 @@
 // Session 4 Data //
 ////////////////////////////////////////////////////////////////////////////////
 var session_4_class_1 = {
-	camp_name: "Art",
-	age_group: "4-8 yrs",
-	days: "M, T, W, Th",
-	time: "1:30 pm - 3:00 pm",
+	camp_name: "Robotics M-W",
+	age_group: "9-13 yrs",
+	days: "M, W",
+	time: "12:00 pm - 1:30 pm",
 	camp_size: "CLOSED"
 };
 
 var session_4_class_2 = {
-	camp_name: "Intro To Shop",
-	age_group: "5-8 yrs",
-	days: "M",
-	time: "1:30 pm - 3:00 pm",
+	camp_name: "Robotics T-Th",
+	age_group: "9-13 yrs",
+	days: "T, Th",
+	time: "12:00 pm - 1:30 pm",
 	camp_size: "CLOSED"
 };
 
