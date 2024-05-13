a9fc9d5cba352724472e9042e94f37493cc9f94f
itrealms <it@rcrealms.org>
2024-05-13 16:15:12 -0700

    updates

--- Pages/Summer Camp/DATA/summer_courses.js
+++ Pages/Summer Camp/DATA/summer_courses.js
@@ -47,17 +47,17 @@ const group2 = {
 		{
 			CourseName: "Beginning Guitar",
 			Description:
-				""
+				"This class is for beginning guitar students with little to no experience. We will begin by learning the instrument's strings, how to tune, learning single notes and chords. By the end of the class we will play a song together as a group and can even sing along if the student likes to sing!!"
 		},
 		{
 			CourseName: "Beginning Ukulele",
 			Description:
-				""
+				"This class is for beginning ukulele players who have little to no experience. This is a fun class where we will be learning all about the ukulele and how to play it. We will begin by learning the instrument strings, how to tune, learning single notes and chords. By the end of the class we will play a song together as a group and can even sing along if the student likes to sing!!"
 		},
 		{
 			CourseName: "Composer Spotlight",
 			Description:
-				""
+				"Meet the masters is a class to take time to get to know some of the greatest musicians and composers. This class will include watching and listening to videos and biographies, having discussions, writing short essays, and doing worksheet."
 		},
 		{
 			CourseName: "English Workshop",
@@ -67,7 +67,7 @@ const group2 = {
 		{
 			CourseName: "Jamily",
 			Description:
-				""
+				"Jamily is for advanced student musicians who want to jam, play music and talk music in a safe and fun environment. Our music groups can be like a second family for us musicians, Welcome to the Jamily!"
 		},
 		{
 			CourseName: "Math Workshop",
@@ -77,12 +77,12 @@ const group2 = {
 		{
 			CourseName: "Music Theory Games",
 			Description:
-				""
+				"Fun games designed for elementary aged students. This class is a fun way to learn about the musical alphabet, dynamics, rhythms, note reading and more. This class is packed full of music education and packed full of fun!"
 		},
 		{
 			CourseName: "Orchestra",
 			Description:
-				""
+				"Orchestra will be for students with little to no experience. Learning to play instruments and read sheet music."
 		},
 		{
 			CourseName: "Science Club",
@@ -92,7 +92,7 @@ const group2 = {
 		{
 			CourseName: "Singing and Signing",
 			Description:
-				""
+				"This will be a fun and educational class that will be applying multiple languages simultaneously. Combining the language of music, sign language, English, and Spanish."
 		},
 		{
 			CourseName: "Social-Emotional Development",
@@ -119,17 +119,17 @@ const group3 = {
 		{
 			CourseName: "Beginning Guitar",
 			Description:
-				""
+				"This class is for beginning guitar students with little to no experience. We will begin by learning the instrument's strings, how to tune, learning single notes and chords. By the end of the class we will play a song together as a group and can even sing along if the student likes to sing!!"
 		},
 		{
 			CourseName: "Beginning Ukulele",
 			Description:
-				""
+				"This class is for beginning ukulele players who have little to no experience. This is a fun class where we will be learning all about the ukulele and how to play it. We will begin by learning the instrument strings, how to tune, learning single notes and chords. By the end of the class we will play a song together as a group and can even sing along if the student likes to sing!!"
 		},
 		{
 			CourseName: "Composer Spotlight",
 			Description:
-				""
+				"Meet the masters is a class to take time to get to know some of the greatest musicians and composers. This class will include watching and listening to videos and biographies, having discussions, writing short essays, and doing worksheet."
 		},
 		{
 			CourseName: "English Workshop",
@@ -139,7 +139,7 @@ const group3 = {
 		{
 			CourseName: "Jamily",
 			Description:
-				""
+				"Jamily is for advanced student musicians who want to jam, play music and talk music in a safe and fun environment. Our music groups can be like a second family for us musicians, Welcome to the Jamily!"
 		},
 		{
 			CourseName: "Math Workshop",
@@ -149,12 +149,12 @@ const group3 = {
 		{
 			CourseName: "Music Theory Games",
 			Description:
-				""
+				"Fun games designed for elementary aged students. This class is a fun way to learn about the musical alphabet, dynamics, rhythms, note reading and more. This class is packed full of music education and packed full of fun!"
 		},
 		{
 			CourseName: "Orchestra",
 			Description:
-				""
+				"Orchestra will be for students with little to no experience. Learning to play instruments and read sheet music."
 		},
 		{
 			CourseName: "Robotics",
@@ -164,7 +164,7 @@ const group3 = {
 		{
 			CourseName: "Singing and Signing",
 			Description:
-				""
+				"This will be a fun and educational class that will be applying multiple languages simultaneously. Combining the language of music, sign language, English, and Spanish."
 		},
 		{
 			CourseName: "Social-Emotional Development",
