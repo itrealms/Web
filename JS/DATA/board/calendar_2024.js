b5fd710b25e1d7520cfa17964bcf4caa2ce587d0
itrealms <115303267+itrealms@users.noreply.github.com>
2024-12-04 12:57:40 -0800

    Update calendar_2024.js

--- JS/DATA/board/calendar_2024.js
+++ JS/DATA/board/calendar_2024.js
@@ -142,7 +142,7 @@ const sep2024 = {"sep" : [
 const oct2024 = {"oct" : [
   {
     "agenda" : "https://drive.google.com/file/d/1vt5w9eXb_zrkkZ-kVXL67u3vggaoSEiV/view",
-    "agenda_date" : "2024-10-24 - Agenda",
+    "agenda_date" : "2024-10-22 - Agenda",
     "minutes" : "#",
     "minutes_date" : ""
   }
@@ -151,7 +151,7 @@ const oct2024 = {"oct" : [
 const nov2024 = {"nov" : [
   {
     "agenda" : "https://drive.google.com/file/d/16iHhkpSUmsfrynrkHzsobFHcfDQQIwyE/view?",
-    "agenda_date" : "2024-11-24 - Agenda",
+    "agenda_date" : "2024-11-26 - Agenda",
     "minutes" : "#",
     "minutes_date" : ""
   }
